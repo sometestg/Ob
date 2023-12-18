0bc8a59c956d2fca2af488d17c8a06d87aaf737e
sometestg <125672753+sometestg@users.noreply.github.com>
2023-12-18 07:31:57 +0700

    Update Protected_1.lua

--- Protected_1.lua
+++ Protected_1.lua
@@ -1,3 +1,3 @@
 if game.PlaceId == 189707 then
-  loadstring(game:HttpGet("https://raw.githubusercontent.com/sometestg/Ob/main/Protected_2.lua"))()
+  loadstring(game:HttpGet("https://raw.githubusercontent.com/sometestg/Ob/main/Protected_2_.lua"))()
 end
